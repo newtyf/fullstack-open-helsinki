2b826dc23b631bd2fb963b09abc23a0bd2d8f3ce
newt <axelskam.123@outlook.es>
2022-11-22 15:29:19 -0500

    part 1 - finish exercise 1.2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom/client";
 
 const Header = (props) => <h1>{props.title}</h1>;
 
+const Part = (props) => (
+  <p>
+    {props.part} {props.exercises}
+  </p>
+);
+
 const Content = (props) => (
-  <React.Fragment>
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
-  </React.Fragment>
+  <div>
+    <Part part={props.part1} exercises={props.exercises1} />
+    <Part part={props.part2} exercises={props.exercises2} />
+    <Part part={props.part3} exercises={props.exercises3} />
+  </div>
 );
 
 const Total = (props) => (
