cf0f4b3bbb6e2ed92058ad702e23fd1d2554ef37
newt <axelskam.123@outlook.es>
2022-11-28 09:59:22 -0500

    part 1 - finish exercise 1.6 - 1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,52 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const Header = (props) => <h1>{props.course.name}</h1>;
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-);
-
-const Content = (props) => (
-  <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-  </div>
-);
-
-const Total = (props) => (
-  <p>
-    Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
-  </p>
-);
-
-const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of component",
-        exercises: 14,
-      },
-    ],
-  };
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good + neutral * 0 + bad * -1) / all;
+  const postives = (good / all) * 100;
 
   return (
-    <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+    <div className="Statistics">
+      <h2>statics</h2>
+      <table border="true">
+        <tbody>
+          <tr align="left">
+            <th>type</th>
+            <th>points</th>
+          </tr>
+          <Static text={"good"} value={good}></Static>
+          <Static text={"neutral"} value={neutral}></Static>
+          <Static text={"bad"} value={bad}></Static>
+          <Static text={"all"} value={all}></Static>
+          <Static text={"average"} value={average}></Static>
+          <Static
+            text={"positives"}
+            value={postives.toString() + " %"}
+          ></Static>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const Static = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Button = ({ text, value, setValue }) => {
+  return (
+    <button
+      onClick={() => {
+        setValue(value + 1);
+      }}
+    >
+      {text}
+    </button>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div className="Feedback">
+      <h2>give feedback</h2>
+      <Button text={"good"} value={good} setValue={setGood} />
+      <Button text={"neutral"} value={neutral} setValue={setNeutral} />
+      <Button text={"bad"} value={bad} setValue={setBad} />
+      {good > 0 || neutral > 0 || bad > 0 ? (
+        <Statistics bad={bad} good={good} neutral={neutral} />
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   );
 };
